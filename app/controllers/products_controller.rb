484c1c0ccc82e0146f99ee68634eb125e9f69ae7
NghiaNTT <nguyen.thi.trong.nghia@framgia.com>
2016-09-16 15:21:51 +0700

    fix UI 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
     @products = Product.in_category params[:category_id]
     @search = @products.ransack params[:q]
     @products = @search.result.order(updated_at: :desc).page(params[:page])
-      .per Settings.per_page
+      .per Settings.products.per_page
   end
 
   def show
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
       @new_review = current_user.reviews.build
     end
       @reviews = @product.reviews.order(updated_at: :desc)
-       .page(params[:page]).per Settings.per_page
+       .page(params[:page]).per Settings.products.per_page
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @search = User.search(params[:q])
     @users = @search.result.order(updated_at: :desc).page(params[:page])
       .per Settings.per_page
+  end
+
+  def show
+    @user ||= current_user
   end
 end
